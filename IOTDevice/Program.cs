b15a47f47dcaa09aaab223e44efca8ec9ede4d6a
Rohan-Deore <deorerohan.2612@gmail.com>
2024-11-16 23:25:35 +0530

    config file added

--- IOTDevice/Program.cs
+++ IOTDevice/Program.cs
@@ -1,9 +1,23 @@
-﻿
+﻿using System.Configuration;
+using Microsoft.Azure.Devices.Client;
+
 public class Program
 {
     public static void Main(string[] args)
     {
         // See https://aka.ms/new-console-template for more information
         Console.WriteLine("Hello, World!");
+        var hubManager = new IOTHubManager();
+        hubManager.SendSampleData();
     }
+}
+
+internal class IOTHubManager
+{
+    public void SendSampleData()
+    {
+        var connectionString = ConfigurationManager.AppSettings["PCIPADDRESS"];
+        var client = DeviceClient.CreateFromConnectionString(".");
+    }
+
 }
