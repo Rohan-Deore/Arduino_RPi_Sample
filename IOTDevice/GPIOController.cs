a9acafa1880cf69ad2c381cf9ea6fb561a7489d6
Rohan-Deore <deorerohan.2612@gmail.com>
2024-11-18 00:01:58 +0530

    Ping out diagram and logs added

--- IOTDevice/GPIOController.cs
+++ IOTDevice/GPIOController.cs
@@ -1,10 +1,13 @@
+using NLog;
 using System.Device.Gpio;
 using System.Threading;
+
 
 namespace IOTDevice
 {
     public class GPIOController
     {
+        private Logger logger = LogManager.GetCurrentClassLogger();
         public void GPIOSample()
         {
             int pin = 18;
@@ -14,6 +17,7 @@ namespace IOTDevice
             int count = 0;
             while (count++ < 10)
             {
+                logger.Debug($"Pin {pin} status : {ledOn}");
                 controller.Write(pin, ((ledOn) ? PinValue.High : PinValue.Low));
                 Thread.Sleep(1000);
                 ledOn = !ledOn;
