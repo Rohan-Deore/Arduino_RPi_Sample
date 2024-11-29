404955247b1597f73065fc471d7be1715a9fa598
Rohan-Deore <deorerohan.2612@gmail.com>
2024-11-29 19:22:50 +0530

    home automation server code implemented

---
+++ Model/IOTServerManager.cs
@@ -0,0 +1,77 @@
+﻿using Azure.Storage.Blobs.Models;
+using Microsoft.Azure.Devices;
+using Newtonsoft.Json;
+using NLog;
+using System.ComponentModel;
+using System.Device.Gpio;
+using System.Text;
+
+
+namespace Model
+{
+    public class IOTServerManager
+    {
+        private Logger logger = LogManager.GetCurrentClassLogger();
+        private ServiceClient? serverClient = null;
+        private string? machineName = string.Empty;
+        private BackgroundWorker worker = new BackgroundWorker();
+        private GpioController controller = new GpioController();
+
+        public IOTServerManager(string connectionString, string machineName)
+        {
+            if (connectionString == null)
+            {
+                logger.Fatal("Connection string is empty");
+                return;
+            }
+
+            if (machineName == null)
+            {
+                machineName = string.Empty;
+                logger.Warn("Machine Name is empty.");
+                return;
+            }
+
+            logger.Debug("IOTDevice manager constructor called");
+            serverClient = ServiceClient.CreateFromConnectionString(connectionString);
+        }
+
+        public async Task Run()
+        {
+            // keep receiving messages.
+            // Get device ID from receiving command so that there will always be correct.
+            while (true)
+            {
+                var fbReceiver = serverClient?.GetFeedbackReceiver();
+                if (fbReceiver == null)
+                {
+                    logger.Error("Service client feedback receiver is null");
+                    break;
+                }
+
+                var token = new CancellationTokenSource();
+                var feedbackBatch = await fbReceiver.ReceiveAsync(token.Token);
+
+                if (feedbackBatch != null)
+                {
+                    logger.Debug("Feedback received:");
+                    foreach (var record in feedbackBatch.Records)
+                    {
+                        logger.Debug($"DeviceId: {record.DeviceId}, Status: {record.StatusCode}, Description: {record.Description}");
+                        ProcessMessage(record);
+                    }
+
+                    // Complete the feedback batch
+                    var cts = new CancellationTokenSource();
+
+                    await fbReceiver.CompleteAsync(feedbackBatch, cts.Token);
+                }
+            }
+        }
+
+        public void ProcessMessage(FeedbackRecord fbRecord)
+        {
+            logger.Info("Data needs to be processed here and send some information to devices.");
+        }
+    }
+}
