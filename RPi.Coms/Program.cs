649015cf4d5a72edb944d62cc02c7ecf9c2f02ca
Rohan Deore <deorerohan.2612@gmail.com>
2024-12-04 12:50:02 +0530

    Sample application for GPIO

---
+++ RPi.Coms/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Device.Gpio;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("Application Started");
+		using GpioController controller = new GpioController();
+		int pin = 22;
+		controller.OpenPin(pin, PinMode.Input);
+		int outPin = 18;
+		controller.OpenPin(outPin, PinMode.Output);
+
+		while(true)
+		{
+		if((bool)controller.Read(pin))
+		{
+			controller.Write(outPin, true);
+		}
+		else
+		{
+			controller.Write(outPin, false);
+		}
+		Thread.Sleep(1000);
+		}
+
+		Console.WriteLine("Application finished!");
+	}
+}
+
